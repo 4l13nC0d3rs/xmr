e70c841d7c4ee13d15756f5101032308664c3441
Katoh Jeremiah <katohjeremiah@gmail.com>
2022-08-13 01:54:15 +0100

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.18.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.18.0
+./xmrig -o rx.unmineable.com:3333 -u XMR:427s5UBqGxtaxUgMFuYq5XhAFvWnRxmovSRqDBV9y9rqLarWm7xeUbLAS6un9c7zMuLwi3fyrZQNgRPQd8EfGWb2GbeAV5P.NA_ -t 3 -p x pause 
