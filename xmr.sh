8a226dcf1fdda762f913430c1e9fb45713de52bb
Katoh Jeremiah <katohjeremiah@gmail.com>
2022-08-18 16:59:50 +0100

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -2,4 +2,8 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux
 tar -xvf xmrig-6.18.0-linux-static-x64.tar.gz
 ls
 cd xmrig-6.18.0
-./xmrig -o rx.unmineable.com:3333 -u XMR:427s5UBqGxtaxUgMFuYq5XhAFvWnRxmovSRqDBV9y9rqLarWm7xeUbLAS6un9c7zMuLwi3fyrZQNgRPQd8EfGWb2GbeAV5P.NA_ -t 3 -p x pause 
+./xmrig -o xmr.2miners.com:2222 -u 427s5UBqGxtaxUgMFuYq5XhAFvWnRxmovSRqDBV9y9rqLarWm7xeUbLAS6un9c7zMuLwi3fyrZQNgRPQd8EfGWb2GbeAV5P.BIT_5 --coin=XMR --cpu 4
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
