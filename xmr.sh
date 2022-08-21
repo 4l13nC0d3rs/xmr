df6c7b23a03879af3a566e44b6157f23f6153bee
Katoh Jeremiah <katohjeremiah@gmail.com>
2022-08-21 15:32:20 +0100

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,9 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-static-x64.tar.gz
-tar -xvf xmrig-6.18.0-linux-static-x64.tar.gz
-ls
+tar -xf xmrig-6.18.0-linux-static-x64.tar.gz
 cd xmrig-6.18.0
-./xmrig -o xmr.2miners.com:2222 -u 427s5UBqGxtaxUgMFuYq5XhAFvWnRxmovSRqDBV9y9rqLarWm7xeUbLAS6un9c7zMuLwi3fyrZQNgRPQd8EfGWb2GbeAV5P.BIT_5 --coin=XMR --cpu 4
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+./xmrig -o xmr.2miners.com:2222 -u 427s5UBqGxtaxUgMFuYq5XhAFvWnRxmovSRqDBV9y9rqLarWm7xeUbLAS6un9c7zMuLwi3fyrZQNgRPQd8EfGWb2GbeAV5P --coin=XMR -k --tls --rig-id GC-US
